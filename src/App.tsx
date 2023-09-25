3a560d7ab8c44f515aebefe2d79a1ec70d91cabb
juanmiguel431 <juanmiguel431@gmail.com>
2023-09-24 22:37:35 -0400

    Firebase dependencies added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { Header } from './components/common';
 
-export default function App() {
+const App: React.FC = () => {
+
+  useEffect(() => {
+    const app = initializeApp({
+      apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+      measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+    });
+
+    const analytics = getAnalytics(app);
+  }, []);
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
@@ -23,3 +41,5 @@ const styles = StyleSheet.create({
     // justifyContent: 'center',
   },
 });
+
+export default App;
